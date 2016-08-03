ea7175025c885f6e673cd0f586276f607a20f85b
RubyRabelle <rabelle.rouser@gmail.com>
2016-08-03 00:40:29 -0500

    first portfolio upload

---
+++ Project1/proj_nav.js
@@ -0,0 +1,6 @@
+document.write("<ul>");
+document.write("<li><a href='proj_contact.html'>Contact</a></li>");
+document.write("<li><a href='proj_faq.html'>FAQ</a></li>");
+document.write("<li><a href='proj_services.html'>Services</a></li>");
+document.write("<li><a href='project1.html'>Home</a></li>");
+document.write("</ul>");
